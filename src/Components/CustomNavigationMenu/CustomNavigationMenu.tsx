0c34a02cc437eb429d7c5f577d150f5ff32a9c35
monoramdas <monoramdas99@gmail.com>
2025-04-27 22:18:10 +0530

    navigation menu

--- src/Components/CustomNavigationMenu/CustomNavigationMenu.tsx
+++ src/Components/CustomNavigationMenu/CustomNavigationMenu.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/Components/ui/navigation-menu";
+import { Navigate, useNavigate } from "react-router";
 // import { Button } from "@shadcn/ui/button";
 
 function CustomNavigationMenu() {
+  const navigate =useNavigate()
   return (
     <nav>
    
@@ -10,23 +12,18 @@ function CustomNavigationMenu() {
         <NavigationMenu>
           <NavigationMenuList className="flex space-x-4">
             <NavigationMenuItem>
-              <NavigationMenuLink href="/" className="text-gray-700 hover:text-black">
+              <NavigationMenuLink onClick={()=>navigate("/")} className="text-gray-700 hover:text-black">
                 Home
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="/projects" className="text-gray-700 hover:text-black">
-                Projects
+              <NavigationMenuLink onClick={()=>navigate("/profile")} className="text-gray-700 hover:text-black">
+                Profile
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink href="/developers" className="text-gray-700 hover:text-black">
+              <NavigationMenuLink  onClick={()=>navigate("/explore")} className="text-gray-700 hover:text-black">
                 Developers
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/about" className="text-gray-700 hover:text-black">
-                About
               </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
